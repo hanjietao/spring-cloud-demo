5148e8bdcdbcd756425a4207ff862908406a23dc
hjtxyr@163.com <hjtxyr@163.com>
2020-01-20 16:45:18 +0800

    commit test

--- demo-eureka-server/src/main/java/com/EurekaApplication.java
+++ demo-eureka-server/src/main/java/com/EurekaApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ *  @author: HanJieTao
+ *  @Date: 2020/1/20 16:44
+ *  @Description: Eureka Server
+ */
+
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
